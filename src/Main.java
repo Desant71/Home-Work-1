9f6c074403eaf11fde4705fdcb4fc42acea6182c
Nikita Filyakin <desant31081993@gmail.com>
2023-02-09 05:00:22 +0300

    1

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[] {5000, 8, 415, 651, 1568});//созд.новый тип данных
-        int max = salesManager.max();//созд. переменную
+        SalesManager salesManager = new SalesManager(new long[] {5000, 8, 415, 651, 1568});//созд.новый тип данных
+        long max = salesManager.max();//созд. переменную
         System.out.println(max);//выводим
     }
 }
