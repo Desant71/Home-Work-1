ed9cd790907fa07cd942bf71619ab45de7c8454b
Desant71 <desant31081992@gmail.com>
2023-02-19 02:50:42 +0300

    min and avarage methods

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        SalesManager salesManager = new SalesManager(new int[] {5000, 8, 415, 651, 1568});//созд.новый тип данных
-        int max = salesManager.max();//созд. переменную
-        System.out.println(max);//выводим
+        SalesManager salesManager = new SalesManager(new int[] {5000, 415, 651, 1568});
+
+        int max = salesManager.max();
+        int min = salesManager.min();
+        int average = salesManager.average();
+
+        System.out.println(max);
+        System.out.println(min);
+        System.out.println(average);
     }
 }
